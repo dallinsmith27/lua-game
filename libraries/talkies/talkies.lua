9288f3612cebd513b8d4ea336c593c8e530b2da8
calhargis <calhargis1@gmail.com>
2022-06-20 14:01:08 -0600

    game.scale connected to talkies.lua

--- libraries/talkies/talkies.lua
+++ libraries/talkies/talkies.lua
@@ -277,13 +277,15 @@ function Talkies.draw()
   love.graphics.setLineWidth(currentDialog.thickness)
 
   -- message box
-  local boxW = windowWidth-(2*currentDialog.padding)
-  local boxH = Talkies.height or (windowHeight/3)-(2*currentDialog.padding)
+  local boxW = (windowWidth-(2*currentDialog.padding)) / game.scale
+  local boxH = (Talkies.height or (windowHeight/3)-(2*currentDialog.padding)) / game.scale
   -- local boxX = currentDialog.padding
   -- local boxY = windowHeight-(boxH+currentDialog.padding)
 
-  local boxX = cam.x - (love.graphics.getWidth() / 2) + 10
-  local boxY = cam.y + (love.graphics.getHeight() / 2) - 300
+  -- local boxX = cam.x - (love.graphics.getWidth() / 2) + 10
+  local boxX = (cam.x * game.scale - love.graphics.getWidth() / 2 + 10) / game.scale
+  local boxY = (cam.y * game.scale + (love.graphics.getHeight() / 2) - 300) / game.scale
+  -- local boxY = cam.y + (love.graphics.getHeight() / 2) - 300
   -- test.num = boxX
 
   -- image
