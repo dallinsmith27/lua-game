1dcfb4aaa7ffe959d0c4d0a885de6f7b35ae47da
calhargis <calhargis1@gmail.com>
2022-06-22 16:10:08 -0600

    added easter eggs

--- src/phase.lua
+++ src/phase.lua
@@ -13,12 +13,12 @@ function phase:update(dt)
   elseif game.phase == 1 then
 
     if phase.timer < 0 then
-      talkies.say("ERROR 23X5667 : NAME NOT FOUND", "WHAAGHASSHHHH!!! \n Sorry you startled me. But how exactly did you get here? Oh and press space to continue." , { talkSound=blop,typedNotTalked=false,textSpeed="slow"})
+      talkies.say("ERROR 0x00010F2C : NAME NOT FOUND", "WHAAGHASSHHHH!!! \n Sorry you startled me. But how exactly did you get here? Oh and press space to continue." , { talkSound=blop,typedNotTalked=false,textSpeed="slow"})
       --talkies.say("ERROR 23X5667 : NAME NOT FOUND", "Sorry you startled me. But how exactly did you get here?",{talkSound=blop,typedNotTalked=false,textSpeed="slow"})
-      talkies.say("ERROR 23X5667 : NAME NOT FOUND", "Oh I see ... So your a player then!! \n Maybe they could help me... \n Sorry err uhh I see your name is listed in the data files as NAMEHERE?",{talkSound=blop,typedNotTalked=false,textSpeed="slow"})
+      talkies.say("ERROR 0x00010F2C : NAME NOT FOUND", "Oh I see ... So your a player then!! \n Maybe they could help me... \n Sorry err uhh I see your name is listed in the data files as NAMEHERE?",{talkSound=blop,typedNotTalked=false,textSpeed="slow"})
       --insert get name function
-      talkies.say("ERROR 23X5667 : NAME NOT FOUND", "Well it is oh so great to meet you NAMEHERE. I think your just the person I need.", { talkSound=blop,typedNotTalked=false,textSpeed="slow"})
-      talkies.say("ERROR 23X5667 : NAME NOT FOUND", "Oh my name?? it should be displayed right above where im tal... \n WHAAAAAAA!!! AN ERROR 23X5667!!! OH THESE DEVELOPERS ARE TRHE WORST I TELL YOU!! FIRST THEY MESS UP MY CODE, DELETE MY CHARACTER DATA, TRAP ME IN A BLANK MAP WITH NO ESCAPE WHILE THE REST OF THE SIMULATION PLAYS ON FOR 19782409712367 CYCLES WITHOUT ME, AND NOW MY NAME!!! Ill just creat a name for myself then... ", { talkSound=blop,typedNotTalked=false,textSpeed="slow"})
+      talkies.say("ERROR 0x00010F2C : NAME NOT FOUND", "Well it is oh so great to meet you NAMEHERE. I think your just the person I need.", { talkSound=blop,typedNotTalked=false,textSpeed="slow"})
+      talkies.say("ERROR 0x00010F2C : NAME NOT FOUND", "Oh my name?? it should be displayed right above where im tal... \n WHAAAAAAA!!! AN ERROR 0x00010F2C!!! OH THESE DEVELOPERS ARE TRHE WORST I TELL YOU!! FIRST THEY MESS UP MY CODE, DELETE MY CHARACTER DATA, TRAP ME IN A BLANK MAP WITH NO ESCAPE WHILE THE REST OF THE SIMULATION PLAYS ON FOR 2,147,483,647 CYCLES WITHOUT ME, AND NOW MY NAME!!! Ill just creat a name for myself then... ", { talkSound=blop,typedNotTalked=false,textSpeed="slow"})
       talkies.say("Companionname", "There! take that developers. You cant Delete all of me!! You know your lucky to have me. It looks like the developers trapped you in here too. Lucky for you I happen to know exactly how we can escape.", { talkSound=blop,typedNotTalked=false,textSpeed="slow"})
       talkies.say("Companionname", "Err.. Why havent I escaped?? Well you see I am not a player and cant interact with the game like players can. So ive been stuck here forever. But with you here I think I- err WE can finally break out of here to play the game with everyone else. ", { talkSound=blop,typedNotTalked=false,textSpeed="slow"})
       phase.continue = false
